f63959c781ef4491b5529c86e40a614fa27bca32
Piotrold <35507293+Piotrold@users.noreply.github.com>
2019-06-24 00:22:25 +0200

    adding drawing-triangle-task

---
+++ src/tasks/Triangle.java
@@ -0,0 +1,19 @@
+package tasks;
+
+public class Triangle {
+    public static void main(String[] args) {
+        draw(20, 10);
+    }
+    private static void draw (int columns, int rows) {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < columns-1; i++) {
+            s.append(" ");
+        }
+        for (int i = 0; i < columns; i++) s.append("*");
+        String result = s.toString();
+        for (int i = 0; i < rows; i++) {
+            if ((columns + 2*i ) > result.length() ) break;
+            System.out.println(result.substring(i, columns + 2*i));
+        }
+    }
+}
